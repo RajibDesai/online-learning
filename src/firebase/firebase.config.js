64ba237c47f1b261dc225fd6448cc294f4561d28
RajibDesai <rajib.desai@gmail.com>
2023-10-07 13:36:36 +0600

    setup project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAx8-JUK42FlV1QiPNjUXYElFWq2NaMuUs",
+  authDomain: "online-learning2.firebaseapp.com",
+  projectId: "online-learning2",
+  storageBucket: "online-learning2.appspot.com",
+  messagingSenderId: "853252189729",
+  appId: "1:853252189729:web:60713e3afe0fdf7384bd60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
